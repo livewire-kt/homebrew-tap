bc46bb50be0897c8ac26845543e09b209d706b98
EricKuck <github@kuckhq.com>
2026-07-12 10:29:12 -0400

    initial commit

---
+++ Casks/livewire.rb
@@ -0,0 +1,19 @@
+cask "livewire" do
+  version "1.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/livewire-kt/livewire/releases/download/v#{version}/Livewire-#{version}.dmg"
+  name "Livewire"
+  desc "Live development bridge that streams app UI to a desktop host"
+  homepage "https://github.com/livewire-kt/livewire"
+
+  depends_on macos: :big_sur
+
+  app "Livewire.app"
+
+  zap trash: [
+    "~/Library/Application Support/Livewire",
+    "~/Library/Caches/com.livewire.host",
+    "~/Library/Preferences/com.livewire.host.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Livewire Homebrew Tap
+
+Homebrew cask for the [Livewire](https://github.com/livewire-kt/livewire) desktop
+app (macOS).
+
+## Install
+
+```bash
+brew install --cask livewire-kt/tap/livewire
+```
+
+or
+
+```bash
+brew tap livewire-kt/tap
+brew install --cask livewire
+```
